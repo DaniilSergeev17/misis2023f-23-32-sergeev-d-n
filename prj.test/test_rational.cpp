7287af4fda3b83b15b0a4a1290d06e73236ce74e
DaniilSergeev17 <81472094+DaniilSergeev17@users.noreply.github.com>
2023-12-04 09:20:17 +0300

    Update test_rational.cpp

--- prj.test/test_rational.cpp
+++ prj.test/test_rational.cpp
@@ -1,33 +1,18 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include <iostream>
-#include <cstdint>
+#include "rational.hpp"
 
-class Rational {
-public:
-    Rational(const int64_t num = 0, const int64_t den = 1);
-    int64_t num() const { return num_; }
-    int64_t den() const { return den_; }
-private:
-    int64_t num_ = 0;
-    int64_t den_ = 1;
-};
 
-Rational::Rational(const int64_t num, const int64_t den)
-    : num_(num), den_(den) {
-    if (0 == den_) {
-        throw std::invalid_argument("Zero denumenator in Rational ctor");
-    }
-}
 
 TEST_CASE("rational ctor") {
     Rational r_def;
-    CHECK(0 == r_def.num());
-    CHECK(1 == r_def.den());
+    CHECK(0 == r_def.num_);
+    CHECK(1 == r_def.den_);
 
     Rational r_int(3);
-    CHECK(3 == r_int.num());
-    CHECK(1 == r_int.den());
+    CHECK(3 == r_int.num_);
+    CHECK(1 == r_int.den_);
 
     CHECK_THROWS(Rational(1, 0));
 }
