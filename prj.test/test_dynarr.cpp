0aa5eaf4de179dcd75d4f789af059b659a981f99
DaniilSergeev17 <81472094+DaniilSergeev17@users.noreply.github.com>
2023-12-13 22:22:12 +0300

    Update test_dynarr.cpp

--- prj.test/test_dynarr.cpp
+++ prj.test/test_dynarr.cpp
@@ -1,7 +1,6 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-#include "dynarr/dynarr.hpp"
-
+#include "prj.lab/dynarr.hpp"
 
 TEST_CASE("dynarr ctor") {
     DynArr arr_def;
@@ -17,4 +16,33 @@ TEST_CASE("dynarr op[]") {
     DynArr arr(size);
     CHECK_EQ(arr[0], 0);
     CHECK_EQ(arr[arr.Size() - 1], 0);
+}
+
+TEST_CASE("dynarr op=") {
+    const int size = 3;
+    DynArr arr_eq(size);
+    DynArr arr_new = arr_eq;
+    CHECK_EQ(arr_new, arr_eq);
+}
+
+TEST_CASE("dynarr resize+") {
+    DynArr arr_bef(3);
+    int res_size = 5;
+    arr_bef.Resize(res_size);
+    CHECK_EQ(arr_bef.Size(), 5);
+}
+
+TEST_CASE("dynarr resize-") {
+    DynArr arr_bef(5);
+    int res_size = 3;
+    arr_bef.Resize(res_size);
+    CHECK_EQ(arr_bef.Size(), 3);
+}
+
+TEST_CASE("dynarr empty&not") {
+    DynArr emp_arr;
+    CHECK_NOTHROW(emp_arr[0]);
+    CHECK_EQ(emp_arr.Size(), 0);
+    DynArr not_emp_arr(2);
+    CHECK_NOTHROW(not_emp_arr[0]);
 }
